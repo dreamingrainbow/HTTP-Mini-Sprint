9eecc48995f068ccf7fd2a39e18e69299872bc12
dreamingrainbow <michaeladennis@yahoo.com>
2017-12-12 23:49:29 -0700

    Update index.js

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -4,4 +4,11 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
     // Add the code for this action
+    const action = () => {
+        const promise = axios.get('http://localhost:3000');
+        return {
+            type: ACTION_TYPE,
+            payload: promise
+        };
+    };
 };
