014747ba0061f9724b74b6385b99b14a7ddaee94
dreamingrainbow <michaeladennis@yahoo.com>
2017-12-12 23:50:19 -0700

    Update index.js

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -5,7 +5,7 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 export const getFriends = () => {
     // Add the code for this action
     const action = () => {
-        const promise = axios.get('http://localhost:5000/Friends');
+        const promise = axios.get('http://localhost:5000/friends');
         return {
             type: ACTION_TYPE,
             payload: promise
